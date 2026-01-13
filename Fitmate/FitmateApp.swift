e735a6b102ed827f32100e77d1b662e7a6d33507
Акыш Акан <akyshakan@gmail.com>
2026-01-13 12:58:35 +0100

    Initial Commit

---
+++ Fitmate/FitmateApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitmateApp.swift
+//  Fitmate
+//
+//  Created by Akan Akysh on 13/01/26.
+//
+
+import SwiftUI
+
+@main
+struct FitmateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
